90eb12f3de2cfb31451b9eab8ce4de2ccc7b4b28
Joseph Gordy <jgordy2424@gmail.com>
2018-04-21 16:27:53 -0400

    Flipped the media query in css to be mobile styling, then moving up to tablet and desktop.

--- src/containers/Splash/index.js
+++ src/containers/Splash/index.js
@@ -4,18 +4,23 @@ import './Splash.css';
 import LinkButton from '../../components/LinkButton';
 
 class Splash extends Component {
+
   render() {
     return(
       <div className='Splash'>
+
         <div className='logo-container'>
-          <img className='Logo' src={require('../../Assets/Logo/PNG/Artboard-1-copy-2Generosity-Logo.png')} alt=""/>
+          <img className='Logo' src={require('../../Assets/Logo/PNG/Artboard-1-copy-2Generosity-Logo.png')} alt="Generosity Market Logo"/>
         </div>
+
         <div className='links'>
           <LinkButton classname={'create-cause'} linkText={'Create a cause'} href={'/'}/>
           <LinkButton classname={'find-cause'} linkText={'Find a cause'} href={'/'}/>
           <LinkButton classname={'sign-in'} linkText={'Sign in'} href={'/'}/>
         </div>
+
         <a href='/' className='sign-up'>Not a member? Sign up here</a>
+
       </div>
     )
   }
