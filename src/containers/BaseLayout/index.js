bea42da436896729a9d898b288e7ba3ddd1883b5
Joseph Gordy <jgordy2424@gmail.com>
2018-04-21 17:39:05 -0400

    Created the top navigation bar and tweaked some of the early navigation logic

--- src/containers/BaseLayout/index.js
+++ src/containers/BaseLayout/index.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
 import { Link } from 'react-router-dom';
+import './index.css';
 
 class BaseLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  navToggle = () => {
+    this.setState({ showMenu: !this.state.showMenu })
+  }
+
+  handleNavigation = (endpoint) => {
+    this.props.history.replace(endpoint);
+    setTimeout(() => this.navToggle(endpoint), 200);
+  }
 
   render() {
+    console.log("props: ", this.props);
     return(
       <div className="BaseLayout">
-        <Link to="/">Home</Link>
-        <Link to='/splash'>Splash</Link>
+
+        <div className='topnav'>
+          <div className='links'>
+            <Link to="/">
+              <i className="fas fa-home"></i>
+            </Link>
+          </div>
+          <div className="menuLogo links">
+            <img src={require('../../Assets/Logo/PNG/Artboard-1-copy-4Generosity-Logo.png')} alt="Generosity Market top menu logo"/>
+          </div>
+          <div className='links'>
+            <div onClick={() => this.setState({showMenu: true})}>
+              <i className="fas fa-bars"></i>
+            </div>
+          </div>
+        </div>
+
+
+        <nav style={this.state.showMenu ? {left: '0%'} : {left: '-100%'}}>
+          <div className='navLinks' onClick={() => this.handleNavigation('/splash')}>Splash</div>
+          <div className='closeMenu' onClick={() => this.setState({showMenu: false})}>
+            <i className="fas fa-times"></i>
+          </div>
+        </nav>
 
         {this.props.children}
-        
+
       </div>
     )
   }
 };
 
-export default BaseLayout;
+export default withRouter(BaseLayout);
