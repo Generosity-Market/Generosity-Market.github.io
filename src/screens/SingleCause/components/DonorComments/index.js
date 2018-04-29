0d9a8ca31bfd2a09488ae4b5fcabc3c335cc7f04
Joseph Gordy <jgordy2424@gmail.com>
2018-04-28 22:26:31 -0400

    Added sample donor data and comments. Next to render them on the screen

--- src/screens/SingleCause/components/DonorComments/index.js
+++ src/screens/SingleCause/components/DonorComments/index.js
@@ -4,9 +4,37 @@ import './DonorComments.css';
 class DonorComments extends Component {
 
   render() {
+    let donorData = [{
+      name: 'Sally Givesalot',
+      comment: 'I love this idea. Happy Birthday!',
+      amount: 100,
+      imageURL: '../../../../Assets/Photography/profile.jpg'
+    }, {
+      name: 'Bob Surrent',
+      comment: 'Happy Birthday Fiona!',
+      amount: 25,
+      imageURL: '../../../../Assets/Photography/profile3.jpg'
+    }, {
+      name: 'Jim Giver',
+      comment: 'See you tonight, Happy Birthday',
+      amount: 50,
+      imageURL: '../../../../Assets/Photography/profile2.jpg'
+    }];
+
+    let donorComments = donorData.map((donor, index) => {
+      return (
+             <div key={index} className="comment-card">
+                <div></div>
+                <div></div>
+             </div>
+           );
+    });
+
     return(
       <div className="DonorComments">
-        <h3>TODO: DonorComments</h3>
+        <h3>Donor Comments</h3>
+
+        {donorComments}
       </div>
     );
   }
