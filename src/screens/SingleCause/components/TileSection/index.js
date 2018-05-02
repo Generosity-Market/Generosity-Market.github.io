8f4486fa62fbf3e33039c63de4c06f7dc2a02912
Joseph Gordy <jgordy2424@gmail.com>
2018-05-02 17:03:09 -0400

    Started on the calulation of the number of tiles needed to raise the fundraising goal.

--- src/screens/SingleCause/components/TileSection/index.js
+++ src/screens/SingleCause/components/TileSection/index.js
@@ -6,7 +6,33 @@ class TileSection extends Component {
 
   // TODO calculate the amount of tiles needed to raise the funding goal.
   calculateTiles = () => {
-    return <Tile amount={50} tileIcon={'Africa.png'}/>;
+    // return <Tile amount={50} tileIcon={'Africa.png'}/>;
+    let tileNumber = 1,
+        amount = this.props.goal;
+
+    while (amount > 0) {
+      if (tileNumber % 21 === 0) {
+        amount = amount - tileNumber;
+        tileNumber++;
+        console.log("--------------------");
+        console.log("amount:: ", amount);
+        console.log("Tile::: ", tileNumber);
+      } else if ((tileNumber === 1) || (tileNumber % 21 === 1)) {
+        amount = amount - tileNumber;
+        tileNumber++;
+        console.log("--------------------");
+        console.log("amount:: ", amount);
+        console.log("Tile::: ", tileNumber);
+      } else {
+        amount = amount - tileNumber;
+        tileNumber++;
+        console.log("--------------------");
+        console.log("amount:: ", amount);
+        console.log("Tile::: ", tileNumber);
+      }
+    };
+    console.log("End Amount: ", amount);
+    console.log("Total Tiles:: ", tileNumber);
   };
 
   render() {
