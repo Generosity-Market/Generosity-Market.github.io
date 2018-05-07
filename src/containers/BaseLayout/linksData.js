2f469a9d8149ba28c70c8c71dd48429663f2ef1e
Joseph Gordy <jgordy2424@gmail.com>
2018-05-07 15:48:49 -0400

    Removed hardcoded navlinks and added a function to create them instead

---
+++ src/containers/BaseLayout/linksData.js
@@ -0,0 +1,18 @@
+const navLinks =
+[{
+  name: 'Splash Page', icon: 'fab fa-cloudsmith', endpoint: '/', color: ''
+}, {
+  name: 'Find A Cause', icon: 'fas fa-donate', endpoint: '/causes', color: ''
+}, {
+  name: 'Create A Cause', icon: 'fas fa-puzzle-piece', endpoint: '/causes/new', color: ''
+}, {
+  name: 'Cause 1', icon: 'fas fa-hands-helping', endpoint: '/cause/1', color: ''
+}, {
+  name: 'Dashboard', icon: 'fas fa-tachometer-alt', endpoint: '/users/1/dashboard', color: ''
+}, {
+  name: 'Organizations', icon: 'fas fa-building', endpoint: '/organizations/1', color: ''
+}, {
+  name: 'Checkout', icon: 'fas fa-shopping-cart', endpoint: '/checkout', color: ''
+}];
+
+export default navLinks;
