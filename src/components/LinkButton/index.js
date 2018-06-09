05a2b14174bef9fd13a4a672bfd8a02bcada3f21
Joseph Gordy <jgordy2424@gmail.com>
2018-06-09 17:47:59 -0400

    Fixed link button to display a regular anchor tag if the href is external and to open it in a new tab/window.

--- src/components/LinkButton/index.js
+++ src/components/LinkButton/index.js
@@ -3,39 +3,33 @@ import { Link } from 'react-router-dom';
 
 export default class LinkButton extends Component {
 
-
-  isExternalLink = (href) => {
-    return href.includes('https://');
-  }
+  isExternalLink = (href) => href.includes('https://');
   // NOTE takes two props 'href' and the 'linkText'
   render() {
     const { href, classname, linkText } = this.props;
-    const linkStyles = {
-        width:          '100%',
-        height:         '100%',
-        display:        'inline-block',
-        padding:        '1.25rem 0rem',
-        cursor:         'pointer',
-        fontSize:       '18px',
-        textTransform:  'uppercase',
-        textDecoration: 'none',
-        textAlign:      'center'
-    }
     let externalLink;
     if (href) {
-    const externalLink = this.isExternalLink(href);
+      externalLink = this.isExternalLink(href);
     }
 
     return (
       <div className="LinkButton">
-
         { externalLink ?
-        <a
-        href={href || '#'} className={classname} style={linkStyles}>{linkText}</a> :
-
-        <Link to={href || '#'} className={classname} style={linkStyles}>{linkText}</Link> }
-
+        <a href={href} className={classname} target="_blank" style={linkStyles}>{linkText}</a> :
+        <Link to={href || '#'} className={classname} style={linkStyles}>{linkText}</Link> }  
       </div>
     );
   }
 };
+
+const linkStyles = {
+    width:          '100%',
+    height:         '100%',
+    display:        'inline-block',
+    padding:        '1.25rem 0rem',
+    cursor:         'pointer',
+    fontSize:       '18px',
+    textTransform:  'uppercase',
+    textDecoration: 'none',
+    textAlign:      'center'
+}
