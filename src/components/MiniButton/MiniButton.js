81fccc623904dcbb959d6ca606182ef71e0a0c08
Joseph Gordy <jgordy2424@gmail.com>
2018-06-22 21:24:36 -0400

    Worked on a small example for including PropTypes in the mini-button component.

--- src/components/MiniButton/MiniButton.js
+++ src/components/MiniButton/MiniButton.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MiniButton = (props) =>
   <div className={`${props.classname} btns`} onClick={(e) => props.onClick(e)}>
     {props.children}
   </div>;
+
+MiniButton.propTypes = {
+  classname: PropTypes.string,
+  onClick: PropTypes.func
+}
 
 export default MiniButton;
