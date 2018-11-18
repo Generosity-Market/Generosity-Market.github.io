a5d5350fea0beaa52fd3b940eca98b108e273347
Joseph Gordy <jgordy2424@gmail.com>
2018-11-17 19:07:28 -0500

    Abstract route info into it's own component with code splitting based on route.

--- src/index.js
+++ src/index.js
@@ -5,27 +5,17 @@ import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 // Redux and React-router
-// TODO import 'Redirect' from RRD when redux/auth/api is implemented
 import { Provider } from 'react-redux';
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { Switch, BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers/reducer';
+// import { TransitionGroup, CSSTransition } from "react-transition-group";
 // import Cookies from 'js-cookie';
 
-// Component
+// Components and Routes
 import BaseLayout from './containers/BaseLayout/BaseLayout';
-import App from './containers/App';
-import Splash from './screens/Splash/Splash';
-import Dashboard from './screens/Dashboard/Dashboard';
-import CauseList from './screens/CauseList/CauseList';
-import CauseForm from './screens/CauseForm/CauseForm';
-import Organization from './screens/Organization/Organization';
-import NewOrgForm from './screens/NewOrgForm/NewOrgForm';
-import SingleCause from './screens/SingleCause/SingleCause';
-import Checkout from './screens/Checkout/Checkout';
-import ThankYou from './screens/ThankYou/ThankYou';
-// import Error404 from './screens/Error404/Error404';
+import Routes from './routes/Routes';
 
 
 // creating the redux store with middleware
@@ -47,17 +37,7 @@ ReactDOM.render(
   <BrowserRouter>
     <BaseLayout>
       <Switch>
-        <Route exact path='/login' component={App} />
-        <Route exact path='/users/:id/dashboard' component={Dashboard} />
-        <Route exact path='/checkout' component={Checkout} />
-        <Route exact path='/thankyou' component={ThankYou} />
-        <Route exact path='/cause/:id' component={SingleCause} />
-        <Route exact path='/causes/new' component={CauseForm} />
-        <Route exact path='/causes' component={CauseList} />
-        <Route exact path='/organizations/new' component={NewOrgForm} />
-        <Route exact path='/organizations/:id' component={Organization} />
-        <Route exact path='/' component={Splash} />
-        {/* <Route component={Error404} /> */}
+        <Routes/>
       </Switch>
     </BaseLayout>
   </BrowserRouter>
---
+++ src/routes/Routes.js
@@ -0,0 +1,81 @@
+import React, { Fragment } from 'react'
+import Loadable from 'react-loadable';
+// TODO import 'Redirect' from RRD when redux/auth/api is implemented
+import { Route } from 'react-router-dom';
+
+// Lazy importing each route
+// TODO create legit Loadable states for each screen
+const Splash = Loadable({
+    loader: () => import('../screens/Splash/Splash'),
+    loading: () => <div>Loading...</div>,
+})
+
+const Dashboard = Loadable({
+    loader: () => import('../screens/Dashboard/Dashboard'),
+    loading: () => <div>Loading...</div>,
+})
+
+const CauseList = Loadable({
+    loader: () => import('../screens/CauseList/CauseList'),
+    loading: () => <div>Loading...</div>,
+})
+
+const SingleCause = Loadable({
+    loader: () => import('../screens/SingleCause/SingleCause'),
+    loading: () => <div>Loading...</div>,
+})
+
+const Checkout = Loadable({
+    loader: () => import('../screens/Checkout/Checkout'),
+    loading: () => <div>Loading...</div>,
+})
+
+const ThankYou = Loadable({
+    loader: () => import('../screens/ThankYou/ThankYou'),
+    loading: () => <div>Loading...</div>,
+})
+
+const Organization = Loadable({
+    loader: () => import('../screens/Organization/Organization'),
+    loading: () => <div>Loading...</div>,
+})
+
+const NewOrgForm = Loadable({
+    loader: () => import('../screens/NewOrgForm/NewOrgForm'),
+    loading: () => <div>Loading...</div>,
+})
+
+const CauseForm = Loadable({
+    loader: () => import('../screens/CauseForm/CauseForm'),
+    loading: () => <div>Loading...</div>,
+})
+
+const App = Loadable({
+    loader: () => import('../containers/App'),
+    loading: () => <div>Loading...</div>,
+})
+
+const Error404 = Loadable({
+    loader: () => import('../screens/Error404/Error404'),
+    loading: () => <div>Loading...</div>,
+})
+
+const Routes = () => {
+    return (
+        <Fragment>
+            <Route exact path='/login' component={App} />
+            <Route exact path='/users/:id/dashboard' component={Dashboard} />
+            <Route exact path='/checkout' component={Checkout} />
+            <Route exact path='/thankyou' component={ThankYou} />
+            <Route exact path='/cause/:id' component={SingleCause} />
+            <Route exact path='/causes/new' component={CauseForm} />
+            <Route exact path='/causes' component={CauseList} />
+            <Route exact path='/organizations/new' component={NewOrgForm} />
+            <Route exact path='/organizations/:id' component={Organization} />
+            <Route exact path='/' component={Splash} />
+            <Route component={Error404} />
+        </Fragment>
+    )
+};
+
+export default Routes;
