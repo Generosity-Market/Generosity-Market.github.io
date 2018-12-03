34de89385e453a683639d16644a9df12cf886e8f
Joseph Gordy <jgordy2424@gmail.com>
2018-12-02 21:00:49 -0500

    Extracted logic for the cause list sliders and headings into it's own component

---
+++ src/screens/CauseList/components/SliderSection/SliderSection.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+
+// Import HOC to see if component is in viewport
+import handleViewport from 'react-in-viewport';
+
+// Component imports
+import CauseTile from '../../../../components/CauseTile/CauseTile';
+import Slider from '../../../../components/Slider/Slider';
+import Heading from '../../../../components/Heading/Heading';
+
+const CauseTileWithLazyLoad = handleViewport(CauseTile);
+
+const SliderSection = ({
+    featured,
+    causeList,
+    causeSelected,
+}) => {
+
+    const isFeatured = (isFeatured) => {
+        return causeList.map(cause => {
+            return (
+                <CauseTileWithLazyLoad
+                    key={cause.id}
+                    cause={cause}
+                    isFeatured={isFeatured}
+                    raised={Number(cause.totalRaised)}
+                    causeSelected={causeSelected}
+                />
+            );
+        });
+    };
+
+    return (
+        <Fragment>
+            <Heading text={'Featured'} />
+            <Slider>
+                {isFeatured(featured)}
+            </Slider>
+        </Fragment>
+    );
+};
+
+export default SliderSection;
