8d87fdbda81c4616bef26494214f0c9c82daa02e
Joseph Gordy <jgordy2424@gmail.com>
2018-12-27 18:48:07 -0500

    Rename App component to Login

---
+++ src/screens/Login/Login.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { register, login } from '../../actions/actions';
+import './Login.css';
+
+
+// import LinkButton from '../../components/LinkButton/LinkButton';
+import ActionButton from '../../components/ActionButton';
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            loggingIn: false
+        }
+    }
+
+    handleState = field => {
+        return (event) => {
+            this.setState({
+                [field]: event.target.value
+            });
+        };
+    };
+
+    handleSubmit = () => {
+        const { login, history } = this.props;
+        const { email, password } = this.state;
+        console.log(this.state);
+        login({ email, password })
+            .then(user => {
+                if (user) history.push(`/users/${user.id}/dashboard`);
+            });
+    }
+
+    render() {
+        return (
+            <div className="Login">
+
+                <div className="sign-in">
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email"
+                        onChange={this.handleState('email')} 
+                        value={this.state.email}
+                        autoFocus
+                    />
+
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Password"
+                        onChange={this.handleState('password')} 
+                        value={this.state.password}
+                    />
+
+                    <div className="submit-buttons">
+                        <ActionButton 
+                            action={this.handleSubmit}
+                            classname='log-in-button' 
+                            actionText='Log In'
+                        />
+                        <ActionButton 
+                            action={this.handleSubmit}
+                            classname='sign-up-button' 
+                            actionText='Sign up'
+                        />
+                    </div>
+                    {/*<a 
+                        href="/causes" 
+                        className="forgot-password"
+                    >
+                        Forgot your password? Reset It Here
+                    </a>*/}
+                </div>
+
+                {/*<h3>OR</h3>*/}
+
+                {/*<LinkButton 
+                    classname="facebook-login" 
+                    href="/causes" 
+                    linkText={'log in with facebook'}
+                />*/}
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = { register, login };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
---
+++ src/screens/Login/Login.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Login.js';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../../reducers/reducer';
+
+let store = createStore(reducers);
+
+describe('<App />', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>, div);
+    });
+});
