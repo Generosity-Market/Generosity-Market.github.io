e41e1fa43b96561160a0a0fc3b52afa5bcd8fcc0
Joseph Gordy <jgordy2424@gmail.com>
2019-01-15 18:49:31 -0500

    Updates to the login/register page including error handling. Preparation for a popup to naviagte once sucessfully logged in

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { register, login } from '../../actions/actions';
+import { withRouter } from 'react-router-dom';
+import { register, login } from '../../actions/user';
 import './Login.css';
 
 
 // import LinkButton from '../../components/LinkButton/LinkButton';
 import ActionButton from '../../components/ActionButton';
+// import Modal from '../../components/Modal/Modal';
 
 class Login extends Component {
     constructor(props) {
@@ -13,63 +15,115 @@ class Login extends Component {
         this.state = {
             email: '',
             password: '',
-            loggingIn: false
+            loggingIn: false,
+            error: null,
+            context: 'login',
+            // modalIsOpen: false,
         }
     }
 
+    componentDidMount() {
+        const { context } = this.props.location.state;
+        if (context === 'register') this.setState({ context: context });
+    }
+
+    // handleOpenModal = () => {
+    //     this.setState({ modalIsOpen: true });
+    // }
+    
+    // handleCloseModal = () => {
+    //     this.setState({ modalIsOpen: false });
+    // }
+
     handleState = field => {
         return (event) => {
+            this.handleError(null);
             this.setState({
                 [field]: event.target.value
             });
         };
     };
 
-    handleSubmit = () => {
-        const { login, history } = this.props;
-        const { email, password } = this.state;
-        
-        console.log(this.state);
-        login({ email, password })
-            .then(user => {
-                if (user) history.push(`/users/${user.id}/dashboard`);
-            });
+    handleError = error => {
+        this.setState({ error });
     }
 
-    render() {
-        return (
-            <div className="Login">
+    handleSubmit = async (action) => {
+        const { history } = this.props;
+        const { email, password } = this.state;
+        this.setState({ loggingIn: true });
 
+        const { user, error } = await action({ email, password });
+
+        if (error) this.handleError(error); this.setState({ loggingIn: false });
+        // TODO Use location state to determine what page navigated to the login page... redirect there...
+        if (user) history.push(`/users/${user.id}/dashboard`);
+    }
+
+    renderActionButton = (context) => {
+        return (
+            <ActionButton
+                action={() => this.handleSubmit(this.props[context])}
+                classname={context === 'register' ? 'sign-up-button' : 'log-in-button'}
+                actionText={context === 'register' ? 'Sign up' : 'Log in'}
+            />
+        )
+    }
+
+
+    render() {
+        const {
+            email,
+            password,
+            error,
+            context,
+        } = this.state;
+
+        const {
+            location: {
+                state: {
+                    from,
+                }
+            }
+        } = this.props;
+
+;        return (
+            <div className="Login">
                 <div className="sign-in">
-                    <input 
-                        type="email" 
-                        name="email" 
+                    <p 
+                        className="error-message" 
+                        style={{ 
+                            backgroundColor: `${error ? 'var(--danger)' : 'transparent'}`,
+                        }}
+                    >
+                        {error}
+                    </p>
+                    
+                    <input
+                        type="email"
+                        name="email"
                         placeholder="Email"
-                        onChange={this.handleState('email')} 
-                        value={this.state.email}
+                        onChange={this.handleState('email')}
+                        value={email}
                         autoFocus
                     />
 
-                    <input 
-                        type="password" 
-                        name="password" 
+                    <input
+                        type="password"
+                        name="password"
                         placeholder="Password"
-                        onChange={this.handleState('password')} 
-                        value={this.state.password}
+                        onChange={this.handleState('password')}
+                        value={password}
                     />
 
                     <div className="submit-buttons">
-                        <ActionButton 
-                            action={this.handleSubmit}
-                            classname='log-in-button' 
-                            actionText='Log In'
-                        />
-                        <ActionButton 
-                            action={this.handleSubmit}
-                            classname='sign-up-button' 
-                            actionText='Sign up'
+                        <ActionButton
+                            action={() => this.handleSubmit(this.props[context])}
+                            classname={context === 'register' ? 'sign-up-button' : 'log-in-button'}
+                            actionText={context === 'register' ? 'Sign up' : 'Log in'}
                         />
                     </div>
+
                     {/*<a 
                         href="/causes" 
                         className="forgot-password"
@@ -78,6 +132,14 @@ class Login extends Component {
                     </a>*/}
                 </div>
 
+                <ActionButton
+                    action={() => this.setState({
+                        context: context === 'register' ? 'login' : 'register',
+                    })}
+                    classname={context === 'register' ? 'sign-up-link' : 'log-in-link'}
+                    actionText={context === 'register' ? 'Already a member? Log in here' : 'Not a member? Sign up here'}
+                />
+
                 {/*<h3>OR</h3>*/}
 
                 {/*<LinkButton 
@@ -85,6 +147,13 @@ class Login extends Component {
                     href="/causes" 
                     linkText={'log in with facebook'}
                 />*/}
+
+                {/*<Modal
+                    isOpen={this.state.modalIsOpen}
+                    handleCloseModal={this.handleCloseModal}
+                >
+                    Modal!!
+                </Modal>*/}
 
             </div>
         );
@@ -97,4 +166,4 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = { register, login };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Login));
