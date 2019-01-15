105cbd103610f83a36c3dc0be9aad9374e1686f8
Joseph Gordy <jgordy2424@gmail.com>
2019-01-15 18:23:08 -0500

    Rename SingleCause to CauseDetail, update routes.js, add Redirects in preparation for authentication

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react';
+import Cookies from 'js-cookie';
 import './Routes.css';
 
 // TODO import 'Redirect' from RRD when redux/auth/api is implemented
@@ -6,6 +7,7 @@ import {
     Switch,
     Route,
     withRouter,
+    Redirect,
 } from 'react-router-dom';
 
 import {
@@ -17,7 +19,7 @@ import {
 const Splash = React.lazy(() => import('../screens/Splash/Splash'));
 const Dashboard = React.lazy(() => import('../screens/Dashboard/Dashboard'));
 const CauseList = React.lazy(() => import('../screens/CauseList/CauseList'));
-const SingleCause = React.lazy(() => import('../screens/SingleCause/SingleCause'));
+const CauseDetail = React.lazy(() => import('../screens/CauseDetail/CauseDetail'));
 const Checkout = React.lazy(() => import('../screens/Checkout/Checkout'));
 const ThankYou = React.lazy(() => import('../screens/ThankYou/ThankYou'));
 const Organization = React.lazy(() => import('../screens/Organization/Organization'));
@@ -25,6 +27,13 @@ const NewOrgForm = React.lazy(() => import('../screens/NewOrgForm/NewOrgForm'));
 const CauseForm = React.lazy(() => import('../screens/CauseForm/CauseForm'));
 const Login = React.lazy(() => import('../screens/Login/Login'));
 const Error404 = React.lazy(() => import('../screens/Error404/Error404'));
+
+// checking to see if there are cookies for authentication
+const loggedIn = () => {
+    return !!Cookies.get('token');
+}
+
+const userCookie = Cookies.getJSON('user');
 
 const Routes = ({ location }) => {
     return (
@@ -34,19 +43,39 @@ const Routes = ({ location }) => {
                 timeout={{ enter: 300, exit: 300 }}
                 classNames={'fade'}
             >
-                <Suspense fallback={<span></span>}>
+                <Suspense fallback={null}>
                     <Switch location={location}>
-                        <Route exact path='/login' component={Login} />
-                        <Route exact path='/users/:id/dashboard' component={Dashboard} />
+
+                        <Route exact path='/login' render={() => (
+                            !loggedIn() || !userCookie ? <Login/> : <Redirect to={`/users/${userCookie.id}/dashboard`} />
+                        )} />
+
+                        <Route exact path='/users/:id/dashboard' render={() => (
+                            loggedIn() ? <Dashboard userData={userCookie} /> : <Redirect to={{pathname: '/login', state: { from: '/users/:id/dashboard'} }} />
+                        )} />
+
                         <Route exact path='/checkout' component={Checkout} />
+
                         <Route exact path='/thankyou' component={ThankYou} />
-                        <Route exact path='/cause/:id' component={SingleCause} />
-                        <Route exact path='/causes/new' component={CauseForm} />
+
+                        <Route exact path='/cause/:id' component={CauseDetail} />
+
+                        <Route exact path='/causes/new' render={() => (
+                            loggedIn() ? <CauseForm/> : <Redirect to={{pathname: '/login', state: { from: '/causes/new'}}}/>
+                        )} />
+
                         <Route exact path='/causes' component={CauseList} />
+
                         <Route exact path='/organizations/new' component={NewOrgForm} />
+
                         <Route exact path='/organizations/:id' component={Organization} />
-                        <Route exact path='/' component={Splash} />
+
+                        <Route exact path='/'  render={() => (
+                            !loggedIn() ? <Splash /> : <Redirect to={{ pathname: `/users/${userCookie.id}/dashboard` }} />
+                        )} />
+
                         <Route component={Error404} />
+
                     </Switch>
                 </Suspense>
             </CSSTransition>
