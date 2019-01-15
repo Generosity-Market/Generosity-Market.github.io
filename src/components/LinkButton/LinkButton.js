a957c86e3f003453b7ef526c78782f328c2efb45
Joseph Gordy <jgordy2424@gmail.com>
2019-01-15 18:29:14 -0500

    Add proptypes to LinkButton and ability to set button context from a prop

--- src/components/LinkButton/LinkButton.js
+++ src/components/LinkButton/LinkButton.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 
-const LinkButton = ({ href, classname, linkText }) => {
+const LinkButton = ({
+  href,
+  classname,
+  linkText,
+  context,
+}) => {
   const isExternalLink = (href) => href.includes('https://');
   let externalLink;
   if (href) {
     externalLink = isExternalLink(href);
   }
 
-  const anchor = 
+  const anchor = (
     <a
       href={href}
       className={classname}
@@ -17,34 +23,62 @@ const LinkButton = ({ href, classname, linkText }) => {
       style={linkStyles}
     >
       {linkText}
-    </a>;
+    </a>
+  );
 
-  const link = 
+  const link = (
     <Link
-      to={href || '#'}
+      to={{
+        pathname: href || '#',
+        state: { context }
+      }}
       className={classname}
       style={linkStyles}
     >
       {linkText}
-    </Link>;
+    </Link>
+  );
 
   return (
     <div className="LinkButton">
-      { externalLink ? anchor : link }
+      {externalLink ? anchor : link}
     </div>
   );
 };
 
 const linkStyles = {
-    width:          '100%',
-    height:         '100%',
-    display:        'inline-block',
-    padding:        '1.25rem 0rem',
-    cursor:         'pointer',
-    fontSize:       '18px',
-    textTransform:  'uppercase',
-    textDecoration: 'none',
-    textAlign:      'center'
+  width: '100%',
+  height: '100%',
+  display: 'inline-block',
+  padding: '1.25rem 0rem',
+  cursor: 'pointer',
+  fontSize: '18px',
+  textTransform: 'uppercase',
+  textDecoration: 'none',
+  textAlign: 'center'
+};
+
+LinkButton.propTypes = {
+  /**
+  * The classname to apply to the root node
+  */
+  classname: PropTypes.string.isRequired,
+	/**
+    * The text to render at the root nodes
+    */
+  linkText: PropTypes.string.isRequired,
+	/**
+    * The href for navigation button click
+    */
+  href: PropTypes.string.isRequired,
+	/**
+    * Context is used to render either the login or signup buttons based on where a user clicks from...
+    */
+  context: PropTypes.oneOf(['register', 'login', null]),
+};
+
+LinkButton.defaultProps = {
+  href: '#',
 };
 
 export default LinkButton;
