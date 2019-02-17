ae3a126b6960a280250d1e2a939928e443a6339f
Joseph Gordy <jgordy2424@gmail.com>
2019-02-16 21:54:12 -0500

    Create service files based on ducks layout

---
+++ src/services/cause.js
@@ -0,0 +1,15 @@
+import {
+    fetchJSONData,
+    postJSONData,
+    postFormData,
+} from 'fetchers/genericFetchers';
+
+import { getLazyImagePlaceholder } from 'fetchers/lazyPlaceholderFetcher';
+
+export const fetchCauseList = () => fetchJSONData('/causes');
+
+export const fetchSingleCause = (id) => fetchJSONData(`/causes/${id}`);
+
+export const submitCauseFormData = (data) => postFormData('/causes/new', data);
+
+export const getPlaceholder = () => getLazyImagePlaceholder();
---
+++ src/services/index.js
@@ -0,0 +1,24 @@
+export {
+    fetchCauseList,
+    fetchSingleCause,
+    submitCauseFormData,
+    getPlaceholder,
+} from './cause';
+
+export {
+    submitPayment,
+} from './donation';
+
+export {
+    userLogin,
+    registerUser,
+    fetchUserData,
+    fetchUserCreatedCauses,
+    fetchUserSupportedCauses,
+} from './user';
+
+export {
+    fetchOrgData,
+    submitOrgFormData,
+    verifyNonProfitStatus,
+} from './organization';
