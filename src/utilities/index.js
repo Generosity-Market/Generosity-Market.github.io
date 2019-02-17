15cfc3c16a2e9da685ccc7d46d29318305474ab7
Joseph Gordy <jgordy2424@gmail.com>
2019-02-16 20:37:53 -0500

    import into utilities/index.js

---
+++ src/utilities/index.js
@@ -0,0 +1,11 @@
+export { default as scrollTo } from './scrollTo';
+export { default as getImageUrl } from './getImageUrl';
+export { default as getIconUrl } from './getIconUrl';
+export { default as sharePage } from './sharePage';
+export { default as removeIndexFromArray } from './removeIndexFromArray';
+export { default as getTotal } from './getTotal';
+export { default as getRandomNumber } from './getRandomNumber';
+export { default as isMobile } from './isMobile';
+export { default as isTestingEnvironment } from './isTestingEnvironment';
+export { default as sortByKey } from './sortByKey';
+export { default as getFormattedDate } from './getFormattedDate';
