5a51ce4acec90c61016b807f6cf3b559fa23ce2d
Joseph Gordy <jgordy2424@gmail.com>
2019-02-25 22:03:57 -0500

    import existing icons and add to fa library initialization

---
+++ src/styles/fontawesome.js
@@ -0,0 +1,45 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import {
+    faArrowAltCircleLeft,
+    faBars,
+    faCamera,
+    faCheckCircle,
+    faChevronLeft,
+    faChevronRight,
+    faDownload,
+    faHome,
+    faInfoCircle,
+    faPlus,
+    faShareAlt,
+    faSquare,
+    faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+    faCreditCard,
+    faImage,
+    faTimesCircle,
+} from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+    faArrowAltCircleLeft,
+    faBars,
+    faCamera,
+    faCheckCircle,
+    faChevronLeft,
+    faChevronRight,
+    faCreditCard,
+    faDownload,
+    faHome,
+    faImage,
+    faInfoCircle,
+    faPlus,
+    faShareAlt,
+    faSquare,
+    faTimes,
+    faTimesCircle,
+    // more icons go here
+);
