747f737d594428fa7ff340ddde6f9513b71ec3fc
Joseph Gordy <jgordy2424@gmail.com>
2019-03-02 12:41:31 -0500

    Fix ImageUploader after FontAwesome changes broke layout of elements

--- src/components/shared/ImageUploader/ImageUploader.js
+++ src/components/shared/ImageUploader/ImageUploader.js
@@ -11,78 +11,109 @@ import {
 export default class ImageUpload extends Component {
 
     createFileInput = (input, URL) => {
-        let iconClass = URL ? 'fas fa-pencil-alt btns' : 'fas fa-plus btns';
+        let iconName = URL ? 'pencil-alt' : 'plus';
         return (
             <label className={URL ? `${input.labelClass} active` : input.labelClass}>
-                <FontAwesome classname={iconClass} />
+                <FontAwesome
+                    icon={iconName}
+                />
                 <input
                     className={input.inputClass}
                     type="file"
                     onChange={(e) => this.props.handleImageChange(e, input.image_type, input.argument)}
-                    onClick={(event) => { event.target.value = null; }} />
+                    onClick={(event) => { event.target.value = null; }}
+                />
             </label>
         );
     }
 
     checkURL = (URL, faClass, roundImage) => {
         if (URL) {
-            return <div className={roundImage ? 'circle' : ''} style={{ backgroundImage: `url(${URL})` }}></div>;
+            return (
+                <div
+                    className={roundImage ? 'circle' : ''}
+                    style={{ backgroundImage: `url(${URL})` }}
+                >
+                </div>
+            );
         } else {
-            return <FontAwesome classname={`${faClass} ${roundImage ? 'circle' : ''}`} />;
+            return (
+                <div className={roundImage ? 'circle' : ''}>
+                    <FontAwesome icon={faClass} />
+                </div>
+            );
         }
     }
 
-    render() {
+    renderCoverPreview = () => {
         const {
-            profileURL,
             coverURL,
-            name,
-            roundImage,
             handleImageChange,
-            handleUpdateState
+            name,
         } = this.props;
 
-        let $profilePreview = this.checkURL(profileURL, 'fas fa-camera', roundImage);
-        let $coverPreview = this.checkURL(coverURL, 'fas fa-image');
+        let $coverPreview = this.checkURL(coverURL, ['far', 'image']);
+
+        return (
+            <div className="coverPreview">
+                <h2 className="banner-heading">{name}</h2>
+                {this.createFileInput(coverInput, coverURL)}
+                {!coverURL && <p>Add Cover Photo</p>}
+                {$coverPreview}
+                {coverURL &&
+                    <MiniButton
+                        classname="delete_cover"
+                        onClick={(e) => handleImageChange(e, 'cover_image', 'coverURL')}
+                    >
+                        <FontAwesome icon={'times'} />
+                    </MiniButton>}
+            </div>
+        );
+    }
+
+    renderProfilePreview = () => {
+        const {
+            handleImageChange,
+            handleUpdateState,
+            profileURL,
+            roundImage,
+        } = this.props;
+
+        let $profilePreview = this.checkURL(profileURL, 'camera', roundImage);
+
+        return (
+            <div className="profilePreview">
+                {this.createFileInput(profileInput, profileURL)}
+                {$profilePreview}
+
+                <MiniButton
+                    classname={profileURL ? 'change_profile_shape active' : 'change_profile_shape'}
+                    onClick={handleUpdateState('roundImage')}
+                >
+                    <FontAwesome
+                        classname={roundImage ? 'fas fa-square' : 'fas fa-circle'}
+                        icon={roundImage ? 'square' : 'circle'}
+                    />
+                </MiniButton>
+
+                <MiniButton
+                    classname={profileURL ? 'delete_profile active' : 'delete_profile'}
+                    onClick={(e) => handleImageChange(e, 'profile_image', 'profileURL')}
+                >
+                    <FontAwesome icon={'times'} />
+                </MiniButton>
+
+            </div>
+        );
+    }
+
+    render() {
 
         return (
             <div className="ImageUploader previewComponent">
                 <div className="Banner">
-
-                    <div className="coverPreview">
-                        <h2 className="banner-heading">{name}</h2>
-                        {this.createFileInput(coverInput, coverURL)}
-                        {!coverURL && <p>Add Cover Photo</p>}
-                        {$coverPreview}
-                        {coverURL &&
-                            <MiniButton
-                                classname="delete_cover"
-                                onClick={(e) => handleImageChange(e, 'cover_image', 'coverURL')}
-                            >
-                                <FontAwesome classname={'fas fa-times'} />
-                            </MiniButton>}
-                    </div>
-
-                    <div className="profilePreview">
-                        {this.createFileInput(profileInput, profileURL)}
-                        {$profilePreview}
-
-                        <MiniButton
-                            classname={profileURL ? 'change_profile_shape active' : 'change_profile_shape'}
-                            onClick={handleUpdateState('roundImage')}
-                        >
-                            <FontAwesome classname={roundImage ? 'fas fa-square' : 'fas fa-circle'} />
-                        </MiniButton>
-
-                        <MiniButton
-                            classname={profileURL ? 'delete_profile active' : 'delete_profile'}
-                            onClick={(e) => handleImageChange(e, 'profile_image', 'profileURL')}
-                        >
-                            <FontAwesome classname={'fas fa-times'} />
-                        </MiniButton>
-
-                    </div>
-
+                    {this.renderCoverPreview()}
+                    {this.renderProfilePreview()}
                 </div>
             </div>
         );
