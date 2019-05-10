359dc2546d7e564a7e5b688b8cc10d62f8b4fb65
Joseph Gordy <jgordy2424@gmail.com>
2019-05-10 11:51:16 -0400

    Refactor fetcher to remove duplicate code

--- src/fetchers/genericFetchers.js
+++ src/fetchers/genericFetchers.js
@@ -3,27 +3,22 @@ import {
 } from 'utilities';
 
 // NOTE add any services and api calls here. Don't expose api keys here. Use env files instead
-const api = endpoint => 'http://localhost:3000/api' + endpoint;
-// const api = endpoint => 'https://gen-mar-poc.herokuapp.com/api' + endpoint;
+const api = endpoint => {
+    const isProdEnv = process.env.NODE_ENV === 'production';
+    const baseUrl = isProdEnv ? 'https://gen-mar-poc.herokuapp.com/api' : 'http://localhost:3000/api';
+    return baseUrl + endpoint;
+};
 
 // TODO handle errors from services instead of logging...Alert system?
-export const fetchJSONData = (URL, args) =>
-    fetch(api(URL), { method: 'GET', body: args })
+const fetcher = (URL, method, options) =>
+    fetch(api(URL), { method, ...options })
         .then(response => response.json())
         .then(data => data)
         /* eslint-disable-next-line no-console */
         .catch(err => !isTestingEnvironment && console.log('Error: ', err));
 
-export const postFormData = (URL, args) =>
-    fetch(api(URL), { method: 'POST', body: args })
-        .then(response => response.json())
-        .then(data => data)
-        /* eslint-disable-next-line no-console */
-        .catch(err => !isTestingEnvironment && console.log('Error: ', err));
+export const fetchJSONData = (URL, args) => fetcher(URL, 'GET', { body: args });
 
-export const postJSONData = (URL, args) =>
-    fetch(api(URL), { method: 'POST', ...args })
-        .then(response => response.json())
-        .then(data => data)
-        /* eslint-disable-next-line no-console */
-        .catch(err => !isTestingEnvironment && console.log('Error: ', err));
+export const postFormData = (URL, args) => fetcher(URL, 'POST', { body: args });
+
+export const postJSONData = (URL, args) => fetcher(URL, 'POST', args);
