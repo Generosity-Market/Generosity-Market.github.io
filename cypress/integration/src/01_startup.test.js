419efd50b573eb8a3f49b4663df4c6c2657cbe85
Joseph Gordy <jgordy2424@gmail.com>
2020-03-09 02:40:46 -0400

    Add login test and fix login buttons

---
+++ cypress/integration/src/01_startup.test.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('App Startup/Login', () => {
+    // beforeEach(() => {
+    //     cy.visit('/');
+    // });
+
+    it('launches', () => {
+        cy.visit('/');
+    });
+
+    it('should click sign in', () => {
+        cy.get('.LinkButton.pale').as('Signin');
+
+        cy.get('@Signin')
+            .should('have.attr', 'href', '/login')
+            .invoke('text')
+            .should('contain', 'Sign in');
+
+        cy.get('@Signin').click();
+
+        cy.location('pathname').should('include', 'login');
+    });
+
+    it('should sign into the application', () => {
+        cy.login('ci@genmar.com', 'ci@genmar');
+
+        cy.wait(1000);
+
+        cy.location('pathname').should('include', 'users');
+    });
+});
