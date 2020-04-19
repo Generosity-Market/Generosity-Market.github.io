61181c4daf166322ce62207654754ad20ac3f8ed
Joseph Gordy <jgordy2424@gmail.com>
2020-04-18 22:48:04 -0400

    Get cart from localStorage on init state

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,7 @@ const initialState = {
         orgList: [],
         selectedOrg: '',
     },
-    cart: [],
+    cart: JSON.parse(localStorage.getItem('cart')) || [],
     // filter: 'all',
     // alert: { type: null, message: null }
 };
