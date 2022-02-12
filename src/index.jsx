e0845b5e39119d9fe6b7d98eb4613630bb468e26
Joseph Gordy <jgordy2424@gmail.com>
2022-02-12 14:13:55 -0500

    Move store logic into store directory

--- src/index.jsx
+++ src/index.jsx
@@ -7,27 +7,12 @@ import registerServiceWorker from './registerServiceWorker';
 // Redux and React-router
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import ReduxPromise from 'redux-promise';
-import rootReducer from './reducers/reducer';
-import initialState from './store/initialState';
 
+import store from 'store';
 
 // Components and Routes
 import { BaseLayout } from 'components/containers';
 import Routes from './routes/Routes';
-
-// Creating the redux store with middleware
-const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(
-        applyMiddleware(ReduxPromise),
-        applyMiddleware(thunk)
-    ),
-);
 
 ReactDOM.render(
     <Provider store={store}>
