024489012fa7e27faeba4e4cd39eba00f6823a97
Joseph Gordy <jgordy2424@gmail.com>
2022-02-21 11:06:03 -0500

    Fix broken profile image in banner component

--- src/components/shared/src/Banner.jsx
+++ src/components/shared/src/Banner.jsx
@@ -7,25 +7,24 @@ import '../styles/Banner.css';
 const Banner = ({
     cover_image,
     heading,
-    profile_mage,
+    profile_image,
     round_image,
-}) =>
-    (
-        <div className='Header'>
-            <div
-                className='hero-image'
-                style={{
-                    backgroundImage: cover_image && `url(${cover_image})`,
-                    backgroundColor: cover_image ? '' : 'var(--black-10)',
-                }}
-            >
-                <h2>{heading && heading}</h2>
-                {/* !props.cover_image && <Glyphicon icon={['far', 'image']} /> */}
-            </div>
-
-            <MainImage profile_image={profile_mage && profile_mage} round_image={round_image} />
+}) => (
+    <div className='Header'>
+        <div
+            className='hero-image'
+            style={{
+                backgroundImage: cover_image && `url(${cover_image})`,
+                backgroundColor: cover_image ? '' : 'var(--black-10)',
+            }}
+        >
+            <h2>{heading && heading}</h2>
+            {/* !props.cover_image && <Glyphicon icon={['far', 'image']} /> */}
         </div>
-    );
+
+        <MainImage profile_image={profile_image && profile_image} round_image={round_image} />
+    </div>
+);
 
 Banner.propTypes = {
     /**
