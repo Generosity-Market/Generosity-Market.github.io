c85e0e16871ccb398a612a04fcf982b2cd6f0c29
Joseph Gordy <jgordy2424@gmail.com>
2022-02-25 23:09:29 -0500

    Have EmptyCart button navigate back a page

--- src/components/containers/Checkout/EmptyCart/EmptyCart.js
+++ src/components/containers/Checkout/EmptyCart/EmptyCart.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import './EmptyCart.css';
 
+import { useNavigate } from 'react-router-dom';
+
 // Shared UI Components
-import { LinkButton } from '@jgordy24/stalls-ui';
+import {
+    // LinkButton,
+    Button,
+} from '@jgordy24/stalls-ui';
 import { TexturedCircle } from 'components/shared';
 
-const EmptyCart = () => (
-    <div className="EmptyCart">
+const EmptyCart = () => {
 
-        <TexturedCircle
-            className="basket-icon"
-            colorMask="rgba(75,183,77,0.07)"
-        >
-            <img src={require('Assets/icons/basket.png')} alt="basket icon" />
-        </TexturedCircle>
+    const navigate = useNavigate();
 
-        <div className="empty-message">
-            <h3>Your basket is empty</h3>
-            <h4>Fill it by supporting some great causes</h4>
+    return (
+        <div className="EmptyCart">
+
+            <TexturedCircle
+                className="basket-icon"
+                colorMask="rgba(75,183,77,0.07)"
+            >
+                <img src={require('Assets/icons/basket.png')} alt="basket icon" />
+            </TexturedCircle>
+
+            <div className="empty-message">
+                <h3>Your basket is empty</h3>
+                <h4>Fill it by supporting some great causes</h4>
+            </div>
+
+            {/* <LinkButton
+                label='Find a Cause'
+                bsStyle='success'
+                href='/causes'
+            /> */}
+
+            <Button
+                label='Go back'
+                bsStyle='success'
+                onClick={() => navigate(-1)}
+            />
+
+
         </div>
-
-        <LinkButton
-            label='Find a Cause'
-            bsStyle='success'
-            href='/causes'
-        />
-
-    </div>
-);
+    );
+};
 
 export default EmptyCart;
